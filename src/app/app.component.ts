6cbe78be22500b31ca25a0256734796976a757b7
Mujib517 <Mujib517@gmail.com>
2018-10-12 06:56:28 +0530

    Expr, prop binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: '<h1>Hello World</h1> <app-home></app-home>',
+  template: `<h1>Hello World</h1> 
+      <h1>{{result?"Passed":"Failed"}}</h1>
+      <button [disabled]="getResult()">Enroll for Course</button>
+      <input type="text" [value]="result"/>
+`,
   selector: 'app-root'
 })
-export class AppComponent { }
+export class AppComponent {
+  private result = true;
+
+  private getResult() {
+    return !this.result;
+  }
+
+  // by default private
+  //0,"",false,undefined,NaN,null
+  //no conditional stmts. if else switch. Ternary prop ?:  && ||
+  // lite weight, no side effects
+  // props, model, state
+  // property binding
+}
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,11 @@ import { Component } from "@angular/core";
   selector: 'app-home'
 })
 export class HomeComponent {
-
+ 
 }
 // bootstrap
 // main.ts --> app.module --> app.component --> view
 // inline ',""/single line
 // multiline
 // separate file
+// Data binding: interpolation(expr), property binding, class binding, stylebinding, two way
