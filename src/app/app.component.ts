3212d4fdb2501ec92b68425933a2ab9bd27fb00d
Mujib517 <Mujib517@gmail.com>
2018-10-15 06:59:25 +0530

    NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,72 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   template: `<h1>Hello World</h1> 
-      <h1>{{result?"Passed":"Failed"}}</h1>
-      <button (click)="onClick()" [disabled]="getResult()">Enroll for Course</button>
-      <input type="text" [value]="result"/>
+  
+  <table border="1" width="100%">
+    <thead>
+      <tr>
+        <th>Value</th>
+        <th>Index</th>
+        <th>First</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr *ngFor="let val of arr;let idx=index;let fst=first">
+        <td>{{val}}</td>
+        <td>{{idx}}</td>
+        <td>{{fst}}</td>
+      </tr>
+    </tbody>
+  </table>
 
-      <h1 [class.red]="count" [style.border]="count?'3px solid green':0">{{count}}</h1>
-
-      <h1 *ngIf="count">NG If Demo</h1>
-
-      <div [ngSwitch]="count">
-        <h1 *ngSwitchCase="3">Low</h1>
-        <h1 *ngSwitchCase="6">Medium</h1>
-        <h1 *ngSwitchCase="9">High</h1>
-        <h1 *ngSwitchDefault>Average</h1>
-      </div>
-
-      <input type="text" (keyup)="onChange($event)" />
-
-      <h1 [ngStyle]="stl" [ngClass]="{red:count>0,green:count<=0}">{{text}}</h1>
-
-
-      <button (click)="inc()">++</button>
-      <button (click)="dec()">--</button>
 `,
   selector: 'app-root'
 })
 export class AppComponent {
-  private result = true;
-  private count = 0;
-  private text = "Hello";
-  //ngClass :string, array,object
-  //private cls: string[] = ["red", "green"];
-  private stl: any = {
-    border: '3px solid black',
-    'border-radius':'15px'
-  };
-
-  private onClick() {
-    console.log("Button clicked");
-  }
-
-  private onChange(evt) {
-    this.text = evt.target.value;
-  }
-
-  private inc() {
-    this.count++;
-  }
-
-  private dec() {
-    this.count--;
-  }
-
-
-  private getResult() {
-    return !this.result;
-  }
-
-  // by default private
-  //0,"",false,undefined,NaN,null
-  //no conditional stmts. if else switch. Ternary prop ?:  && ||
-  // lite weight, no side effects
-  // props, model, state
-  // property binding
-  // SOA
-  //class binding
-  //directives: ngIf,ngClass, ngStyle, ngSwitch,ngFor
+  arr: number[] = [1, 2, 3, 4, 5, 6]; //type inference
 }
 
+//2 cat: Structural Directives (*), Attribute directives
 
