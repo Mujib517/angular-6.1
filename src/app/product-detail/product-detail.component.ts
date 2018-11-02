7f60bd68afa499014ec234ed54bcaddcd3ef23e2
Mujib517 <Mujib517@gmail.com>
2018-11-02 06:34:37 +0530

    Tabs

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -23,8 +23,16 @@ import { ActivatedRoute } from "@angular/router";
   </div>
   </div>
   <br/>
-  <app-product-more-info [avgRating]="product.avgRating"></app-product-more-info>
-  <app-product-reviews [reviews]="product.reviews"></app-product-reviews>
+  <ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link" [ngClass]="activeTab===0?'active':''" (click)="changeTab(0)">Reviews</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" [ngClass]="activeTab===1?'active':''"  (click)="changeTab(1)">More</a>
+  </li>
+</ul>
+  <app-product-more-info *ngIf="activeTab===1" [avgRating]="product.avgRating"></app-product-more-info>
+  <app-product-reviews *ngIf="activeTab===0" [reviews]="product.reviews"></app-product-reviews>
   `
 
 })
@@ -32,11 +40,16 @@ export class ProductDetailComponent {
 
   //? safe navigation operator
   product: any = {};
+  activeTab: number = 0;
 
   constructor(svc: ProductService, route: ActivatedRoute) {
     const id = route.snapshot.params.id;
 
     svc.getById(id)
       .subscribe(res => this.product = res, err => console.log(err));
+  }
+
+  changeTab(val) {
+    this.activeTab = val;
   }
 }
