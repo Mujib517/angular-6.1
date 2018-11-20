bd8c4a505ff035e7c9619d7a5b3073a3cc745e44
Mujib517 <Mujib517@gmail.com>
2018-11-20 07:03:44 +0530

    Change detection

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,33 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  template: '<h1>Contact Page</h1>',
-  selector: 'app-contact'
+  selector: 'app-contact',
+  template: `<h1>Contact Page</h1>
+    <h1>{{count}}</h1>
+
+    <button (click)="reset()">Reset</button>
+    <button (click)="call()">Call API</button>
+  `,
 })
-export class ContactComponent { }
+export class ContactComponent {
+  //CD. 
+  // Asynchronous operation completion
+  // DB,File,Events, Timers
+  count: number = 0;
+
+  constructor(private http: HttpClient) {
+    setInterval(() => ++this.count, 1000);
+  }
+
+  reset() {
+    this.count = 0;
+  }
+
+  call() {
+    setInterval(() => ++this.count, 1000);
+    this.http.get('api/products').
+      subscribe(res => this.count = 100);
+  }
+
+}
